ed7576e3e687a0358f71ab47b8baeb58ee1f1833
Leonardo Machado <leonardomachadosi@hotmail.com>
2018-11-20 11:50:28 -0200

    subindo

--- src/main/java/br/ufma/lsdi/model/domain/interscity/Capability.java
+++ src/main/java/br/ufma/lsdi/model/domain/interscity/Capability.java
@@ -5,18 +5,29 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.*;
 import java.io.Serializable;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 @JsonIgnoreProperties
+@Entity
+@Table(name = "AL02_CAPACIDADE")
+@SequenceGenerator(sequenceName = "AL02_CAPACIDADE_SEQ", name = "AL02_CAPACIDADE_SEQ", allocationSize = 1)
 public class Capability implements Serializable {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "AL02_CAPACIDADE_SEQ")
+    @Column(name = "AL02_COD_CAPACIDADE")
     private Long id;
+    @Column(name = "AL02_NAME")
     private String name;
+    @Column(name = "AL02_FUNCTION")
     private int function;
+    @Column(name = "AL02_DESCRIPTION")
     private String description;
+    @Column(name = "AL02_CAPABILITYTYPE")
     private String capabilityType;
 
 }
--- src/main/java/br/ufma/lsdi/model/domain/interscity/Resource.java
+++ src/main/java/br/ufma/lsdi/model/domain/interscity/Resource.java
@@ -5,6 +5,7 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.*;
 import java.io.Serializable;
 import java.util.List;
 
@@ -12,22 +13,44 @@ import java.util.List;
 @AllArgsConstructor
 @NoArgsConstructor
 @JsonIgnoreProperties(ignoreUnknown = true)
+@Entity
+@Table(name = "AL01_RECURSO")
+@SequenceGenerator(sequenceName = "AL01_RECURSO_SEQ", name = "AL01_RECURSO_SEQ", allocationSize = 1)
 public class Resource implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "AL01_RECURSO_SEQ")
+    @Column(name = "AL01_cod_recurso")
     private Integer id;
+    @Column(name = "AL01_URI")
     private Integer uri;
+    @Column(name = "AL01_CREATED_AT")
     private String createdAt;
+    @Column(name = "AL01_UPDATED_AT")
     private String updatedAt;
+    @Column(name = "AL01_LAT")
     private Double lat;
+    @Column(name = "AL01_LON")
     private Double lon;
+    @Column(name = "AL01_STATUS")
     private String status;
+    @Column(name = "AL01_COLLECT_INTERVAL")
     private String collectInterval;
+    @Column(name = "AL01_DESCRIPTION")
     private String description;
+    @Column(name = "AL01_UUID")
     private String uuid;
+    @Column(name = "AL01_CITY")
     private String city;
+    @Column(name = "AL01_NEIGHBORHOOD")
     private String neighborhood;
+    @Column(name = "AL01_STATE")
     private String state;
+    @Column(name = "AL01_POSTAL_CODE")
     private String postalCode;
+    @Column(name = "AL01_COUNTRY")
     private String country;
+    @Transient
     private List<String> capabilities;
 
 }
