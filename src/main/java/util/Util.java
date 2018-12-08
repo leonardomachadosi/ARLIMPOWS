943d8b0bb6b8a3c8a5a4de4579c75ecc813d52b1
Ivan-Rodrigues <ivanrmoura9@gmail.com>
2018-12-07 22:39:47 -0300

    .

--- src/main/java/util/Util.java
+++ src/main/java/util/Util.java
@@ -67,7 +67,6 @@ public class Util {
     }
 
 
-    private static final String path = "C:\\arquivos";
     public static void gravarArquivo(String nomeArquivo, String dados) throws IOException {
         try{
             // Cria arquivo
